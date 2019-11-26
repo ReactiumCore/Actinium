d8310c621a709246bce3e1b0c32cea7488f7b78a
John Dillick <john@dillick.us>
2019-11-26 16:56:04 -0500

    Document test harness.

--- .core/lib/harness.js
+++ .core/lib/harness.js
@@ -15,6 +15,46 @@ const Harness = {
         }
     },
 
+    /**
+ * @api {Function} Actinium.Harness.test(description,cb,setup,teardown) Actinium.Harness.test()
+ * @apiVersion 3.1.2
+ * @apiGroup Actinium
+ * @apiName Actinium.Harness.test
+ * @apiDescription Add a local development functional test, to be run at startup of Actinium. Your test callback will be passed
+ the node.js 'assert' object.
+ * @apiParam {String} description Describe the tests run.
+ * @apiParam {AsyncFunction} cb the test body
+ * @apiParam {AsyncFunction} setup setup to be performed before the test
+ * @apiParam {AsyncFunction} teardown work to be performed after the test to clean up
+ * @apiExample Example Usage:
+Actinium.Harness.test('My Test', async assert => {
+    assert(false, 'should be true');
+});
+
+// Example with setup / teardown
+const setup = async () => {
+    const user = new Parse.User();
+        user.set("username", "myName");
+        user.set("password", ";lajksdf;lajsdf");
+        user.set("email", "email@example.com");
+    await user.save(null, { useMasterKey: true });
+    await Actinium.Roles.User.add(user.id, 'super-admin', { useMasterKey: true });
+};
+const teardown = async () => {
+    const query = new Parse.Query('_User');
+    query.equalTo('username', 'myName');
+    const user = await query.first({ useMasterKey: true });
+    await user.destroy({ useMasterKey: true });
+};
+Actinium.Harness.test('My Test', async assert => {
+    const query = new Parse.Query('_User');
+    query.equalTo('username', 'myName');
+    const user = await query.first({ useMasterKey: true });
+
+    assert(user, 'User myName should exist.');
+}, setup, teardown);
+
+ */
     test: async (description, cb, setup, teardown) => {
         const desc = slugify(description);
         const assert = require('assert');
