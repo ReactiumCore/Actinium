76190789262dd8401510979d1ef1fe364d35dfe2
John Dillick <john@dillick.us>
2020-01-23 14:31:49 -0500

    Adds content type namespace for type uuid generation.

---
+++ .core/plugin/type/enums.js
@@ -0,0 +1,20 @@
+const UNINSTALLED_NAMESPACE = '9f85eb4d-777b-4213-b039-fced11c2dbae';
+const ID_NAMESPACE = ENV.CONTENT_NAMESPACE || UNINSTALLED_NAMESPACE;
+const PLUGIN_BLUEPRINTS = require('./blueprints');
+const PLUGIN_ROUTES = require('./routes');
+const PLUGIN = {
+    ID: 'Type',
+    name: 'Content Types',
+    meta: {
+        builtIn: true,
+        group: 'core',
+        namespace: ID_NAMESPACE,
+    },
+};
+
+module.exports = {
+    UNINSTALLED_NAMESPACE,
+    PLUGIN_BLUEPRINTS,
+    PLUGIN_ROUTES,
+    PLUGIN,
+};
--- .core/plugin/type/type-plugin.js
+++ .core/plugin/type/type-plugin.js
@@ -1,7 +1,8 @@
 const op = require('object-path');
 const _ = require('underscore');
-const uuid = require('uuid/v4');
+const uuidv5 = require('uuid/v5');
 const slugify = require('slugify');
+const chalk = require('chalk');
 
 const {
     CloudHasCapabilities,
@@ -9,19 +10,40 @@ const {
     CloudRunOptions,
 } = require(`${ACTINIUM_DIR}/lib/utils`);
 
-const PLUGIN_BLUEPRINTS = require('./blueprints');
-const PLUGIN_ROUTES = require('./routes');
-const PLUGIN = {
-    ID: 'Type',
-    name: 'Content Types',
-    meta: {
-        builtIn: true,
-        group: 'core',
-    },
-};
+const {
+    UNINSTALLED_NAMESPACE,
+    PLUGIN_BLUEPRINTS,
+    PLUGIN_ROUTES,
+    PLUGIN,
+} = require('./enums');
+
 const COLLECTION = PLUGIN.ID;
 
 Actinium.Plugin.register(PLUGIN, true);
+
+const getNamespace = () => {
+    return (
+        op.get(Actinium.Plugin.get(PLUGIN.ID), 'meta.namespace') ||
+        UNINSTALLED_NAMESPACE
+    );
+};
+
+Actinium.Hook.register('start', async () => {
+    if (Actinium.Plugin.isActive(PLUGIN.ID)) {
+        const namespace = getNamespace();
+        if (namespace === UNINSTALLED_NAMESPACE) {
+            LOG('');
+            LOG(
+                chalk.cyan.bold('Warning:'),
+                'It appears you have not set the ID_NAMESPACE to a unique random uuid/v4. The default will be used and your content ids will not be unique!',
+            );
+            LOG(
+                chalk.cyan(`  PLUGINS[${PLUGIN.ID}].meta.namespace → `),
+                namespace,
+            );
+        }
+    }
+});
 
 // Register Blueprints
 Actinium.Hook.register(
@@ -93,13 +115,13 @@ Actinium.Cloud.define(PLUGIN.ID, 'types', async req => {
     let pages = 0,
         total = 0;
 
-    let { page = 0, limit = 1000 } = req.params;
+    let { page = 0, limit = 1000, refresh = false } = req.params;
 
     page = Math.max(page, 0);
     limit = Math.min(limit, 1000);
     let cacheKey = ['types', page, limit, 'types'];
 
-    let types = page < 1 ? Actinium.Cache.get(cacheKey, []) : [];
+    let types = page < 1 && !refresh ? Actinium.Cache.get(cacheKey, []) : [];
 
     if (types.length < 1) {
         const skip = page > 0 ? page * limit - limit : 0;
@@ -127,8 +149,9 @@ Actinium.Cloud.define(PLUGIN.ID, 'types', async req => {
         }
         types = types.map(contentType => {
             const obj = contentType.toJSON();
-            const { type, uuid } = obj;
-            return { type, uuid };
+            const { type, uuid, meta = {} } = obj;
+            const label = op.get(meta, 'label', type);
+            return { type, uuid, label };
         });
 
         Actinium.Cache.set(cacheKey, types, 20000);
@@ -157,12 +180,16 @@ Actinium.Cloud.define(PLUGIN.ID, 'type-create', async req => {
     const contentType = new Parse.Object(COLLECTION);
 
     const { type, fields = {} } = req.params;
-
     if (!type) throw new Error('type parameter required.');
+    const machineName = slugify(type).toLowerCase();
 
-    contentType.set('uuid', uuid());
-    contentType.set('type', op.get(req.params, 'type'));
+    contentType.set('uuid', uuidv5(machineName, getNamespace()));
+    contentType.set('type', machineName);
     contentType.set('fields', op.get(req.params, 'fields'));
+    contentType.set('meta', {
+        ...op.get(req.params, 'meta', {}),
+        label: type,
+    });
     contentType.set(
         'regions',
         op.get(req.params, 'regions', {
@@ -179,13 +206,16 @@ Actinium.Cloud.define(PLUGIN.ID, 'type-create', async req => {
 });
 
 Actinium.Cloud.define(PLUGIN.ID, 'type-retrieve', async req => {
+    const machineName = op.get(req.params, 'machineName');
+    const namespace = op.get(req.params, 'namespace', getNamespace());
+    const uuid = machineName
+        ? uuidv5(machineName, namespace)
+        : op.get(req.params, 'uuid');
+
+    if (!uuid) throw new Error('uuid or machineName parameter required.');
+
     const options = CloudCapOptions(req, [`${COLLECTION}.retrieve`]);
     const query = new Parse.Query(COLLECTION);
-
-    const { uuid } = req.params;
-
-    if (!uuid) throw new Error('uuid parameter required.');
-
     query.equalTo('uuid', uuid);
     const contentType = await query.first(options);
 
@@ -195,20 +225,29 @@ Actinium.Cloud.define(PLUGIN.ID, 'type-retrieve', async req => {
 });
 
 Actinium.Cloud.define(PLUGIN.ID, 'type-update', async req => {
+    const machineName = op.get(req.params, 'machineName');
+    const namespace = op.get(req.params, 'namespace', getNamespace());
+    const uuid = machineName
+        ? uuidv5(machineName, namespace)
+        : op.get(req.params, 'uuid');
+    const { type, fields = {} } = req.params;
+    if (!uuid) throw new Error('uuid or machineName parameter required.');
+
     const options = CloudCapOptions(req, [`${COLLECTION}.update`]);
     const query = new Parse.Query(COLLECTION);
-
-    const { uuid, type, fields = {} } = req.params;
-
-    if (!uuid) throw new Error('uuid parameter required.');
-    if (!type) throw new Error('type parameter required.');
-
     query.equalTo('uuid', uuid);
     const contentType = await query.first(options);
 
     if (!contentType) throw new Error('Unable to find type.');
 
-    contentType.set('type', type);
+    let meta = contentType.get('meta') || {};
+    meta = {
+        ...meta,
+        ...op.get(req.params, 'meta'),
+        ...(type ? { label: type } : {}),
+    };
+
+    contentType.set('meta', meta);
     contentType.set('fields', fields);
     contentType.set(
         'regions',
@@ -226,12 +265,15 @@ Actinium.Cloud.define(PLUGIN.ID, 'type-update', async req => {
 });
 
 Actinium.Cloud.define(PLUGIN.ID, 'type-delete', async req => {
+    const machineName = op.get(req.params, 'machineName');
+    const namespace = op.get(req.params, 'namespace', getNamespace());
+    const uuid = machineName
+        ? uuidv5(machineName, namespace)
+        : op.get(req.params, 'uuid');
+    if (!uuid) throw new Error('uuid or machineName parameter required.');
+
     const options = CloudCapOptions(req, [`${COLLECTION}.delete`]);
     const query = new Parse.Query(COLLECTION);
-
-    const { uuid } = req.params;
-
-    if (!uuid) throw new Error('uuid parameter required.');
 
     query.equalTo('uuid', uuid);
     const contentType = await query.first(options);
