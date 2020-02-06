5304bf008dbb86ae500a8a697e4ca1d688e7d247
John Dillick <john@dillick.us>
2020-02-06 16:08:09 -0500

    Update slugify.js

--- .core/lib/utils/slugify.js
+++ .core/lib/utils/slugify.js
@@ -1,8 +1,10 @@
-const slugify = name =>
-    require('slugify')(name, {
+const slugify = name => {
+    if (!name) return '';
+    return require('slugify')(name, {
         replacement: '_', // replace spaces with replacement
         remove: /[^A-Za-z0-9_\s]/g, // regex to remove characters
         lower: true, // result in lower case
     });
+};
 
 module.exports = slugify;
