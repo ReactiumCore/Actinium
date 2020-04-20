d2220f2ce9ed6f99b39639afa06bf59ee034d4d0
Cam Tullos <cam@tullos.ninja>
2020-04-20 15:12:08 -0400

    Added arcli version function.

---
+++ .core/.cli/commands/version/index.js
@@ -0,0 +1,29 @@
+const chalk = require('chalk');
+const path = require('path');
+const op = require('object-path');
+
+const NAME = 'version';
+const DESC = 'The current Reactium Core version';
+
+const ACTION = ({ opt, props }) => {
+    const configPath = path.normalize(
+        path.join(props.cwd, '/.core', 'actinium-config'),
+    );
+    const actiniumConfig = require(configPath);
+    console.log(
+        chalk.cyan('Actinium:'),
+        chalk.magenta(op.get(actiniumConfig, 'version')),
+    );
+    console.log('');
+};
+
+const COMMAND = ({ program, props }) =>
+    program
+        .command(NAME)
+        .description(DESC)
+        .action(opt => ACTION({ opt, props }));
+
+module.exports = {
+    COMMAND,
+    NAME,
+};
