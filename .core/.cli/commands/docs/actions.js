65353bb7514346c21d86d169e09abea4ae928319
John Dillick <john@dillick.us>
2020-05-20 18:10:38 -0400

    Fix arcli docs command, which is unhappy with @atomic-reactor/cli

--- .core/.cli/commands/docs/actions.js
+++ .core/.cli/commands/docs/actions.js
@@ -1,13 +1,13 @@
-
-const path       = require('path');
-const chalk      = require('chalk');
-const fs         = require('fs-extra');
-const _          = require('underscore');
-const op         = require('object-path');
+const path = require('path');
+const chalk = require('chalk');
+const fs = require('fs-extra');
+const _ = require('underscore');
+const op = require('object-path');
 const { createDoc } = require('apidoc');
+const globby = require('globby');
 
-module.exports = (spinner) => {
-    const message = (text) => {
+module.exports = spinner => {
+    const message = text => {
         if (spinner) {
             spinner.text = text;
         }
@@ -15,17 +15,50 @@ module.exports = (spinner) => {
 
     return {
         create: async ({ action, params, props }) => {
-            const { src, dest, verbose } = params;
-
-            await createDoc({
-                src,
-                dest,
-                lineEnding: '\n',
-                debug: verbose,
-                verbose,
-            })
-
             message(`Creating ${chalk.cyan('docs')}...`);
+
+            let { src, dest, verbose } = params;
+
+            src = _.flatten(
+                src.map(search => {
+                    if (search === 'node_modules/@atomic-reactor') {
+                        const globRoot = path
+                            .resolve(process.cwd(), search)
+                            .replace(/\\/g, '/');
+
+                        const globs = [
+                            `${globRoot}/**/src/*.js`,
+                            `${globRoot}/**/lib/*.js`,
+                            `!${globRoot}/**/node_modules/**/*`,
+                            `!${globRoot}/cli/**/*`,
+                        ];
+
+                        return _.uniq(
+                            globby
+                                .sync(globs)
+                                .map(fn =>
+                                    path.dirname(fn).replace(globRoot, ''),
+                                ),
+                        ).map(p => `${search}${p}`);
+                    }
+                    return search;
+                }),
+            );
+
+            dest = String(dest)
+                .replace(/ /gi, '')
+                .split(',');
+            dest = _.flatten([dest]);
+
+            dest.forEach(d => {
+                createDoc({
+                    src,
+                    dest: d,
+                    lineEnding: '\n',
+                    debug: verbose,
+                    verbose,
+                });
+            });
 
             return Promise.resolve({ action, status: 200 });
         },
