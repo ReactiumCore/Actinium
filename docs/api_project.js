13e3c15b6898117ce6c8dc142e9c8c4948939177
John Dillick <john@dillick.us>
2020-05-20 18:11:25 -0400

    Gen docs after fixing command.

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-05-06T18:09:15.821Z",
+    "time": "2020-05-20T22:08:51.869Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
