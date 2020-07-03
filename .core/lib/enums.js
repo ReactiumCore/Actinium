53913e891894b2343c042fae55dae67f46ed2324
John Dillick <john@dillick.us>
2020-07-03 14:24:15 -0400

    Add Actinium log levels.

--- .core/lib/enums.js
+++ .core/lib/enums.js
@@ -1,4 +1,4 @@
-module.exports = {
+const Enums = {
     cache: {
         dataLoading: 1000 * 90,
     },
@@ -9,4 +9,27 @@ module.exports = {
         low: 500,
         lowest: 1000,
     },
+    weight: {
+        highest: 1000,
+        high: 500,
+        neutral: 0,
+        low: -500,
+        lowest: -1000,
+    },
+    logLevels: {
+        DEBUG: 1000,
+        INFO: 500,
+        BOOT: 0,
+        WARN: -500,
+        ERROR: -1000,
+    },
+    parseLogLevels: {
+        DEBUG: 'verbose',
+        INFO: 'info',
+        BOOT: 'error',
+        WARN: 'error',
+        ERROR: 'error',
+    },
 };
+
+module.exports = Enums;
