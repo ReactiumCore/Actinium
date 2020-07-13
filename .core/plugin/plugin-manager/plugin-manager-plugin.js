ee6f4c7b06cccc2a2ed74b84384279fd5af74f72
John Dillick <john@dillick.us>
2020-07-13 17:35:42 -0400

    Update plugin-manager-plugin.js

--- .core/plugin/plugin-manager/plugin-manager-plugin.js
+++ .core/plugin/plugin-manager/plugin-manager-plugin.js
@@ -1,7 +1,6 @@
 const PLUGIN_ROUTES = require('./routes');
 const PLUGIN_BLUEPRINTS = require('./blueprints');
 const registerBlueprints = (reg = true) => ({ ID }) => {
-    if (ID && ID !== PLUGIN.ID) return;
     if (reg === true)
         PLUGIN_BLUEPRINTS.forEach(bp => Actinium.Blueprint.register(bp.ID, bp));
     else PLUGIN_BLUEPRINTS.forEach(bp => Actinium.Blueprint.unregister(bp.ID));
@@ -9,12 +8,6 @@ const registerBlueprints = (reg = true) => ({ ID }) => {
 
 // Start: Blueprints
 Actinium.Hook.register('start', registerBlueprints(true));
-
-// Activate: Blueprints
-Actinium.Hook.register('activate', registerBlueprints(true));
-
-// Deactivate: Blueprints
-Actinium.Hook.register('deactivate', registerBlueprints(false));
 
 // Register Routes
 Actinium.Hook.register('route-defaults', routes => {
