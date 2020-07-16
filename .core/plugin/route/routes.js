2fbe5268ffb252ce3cbd6427009c8fb2bdaa2503
Cam Tullos <cam@tullos.ninja>
2020-07-15 21:23:40 -0400

    Update default routes.

--- .core/plugin/route/routes.js
+++ .core/plugin/route/routes.js
@@ -1,23 +1,7 @@
 module.exports = [
     {
-        route: '/',
-        blueprint: 'Admin',
-        meta: {
-            builtIn: true,
-        },
-        capabilities: ['admin-ui.view'],
-    },
-    {
         route: '/admin',
         blueprint: 'Admin',
-        meta: {
-            builtIn: true,
-        },
-        capabilities: ['admin-ui.view'],
-    },
-    {
-        route: '/admin/profile',
-        blueprint: 'Profile',
         meta: {
             builtIn: true,
         },
