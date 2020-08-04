5a7b4465b0fd466e5320f404aba0d20999e5ac35
John Dillick <john@dillick.us>
2020-08-04 13:55:40 -0400

    Update content SDK to use REST access permissions for ACLs.

--- .core/plugin/content/sdk.js
+++ .core/plugin/content/sdk.js
@@ -1173,8 +1173,8 @@ Content.create = async (params, options) => {
     const permissions = op.get(params, 'permissions', []);
     const groupACL = await Actinium.Utils.CloudACL(
         permissions,
-        `${collection}.retrieveAny`, // read
-        `${collection}.updateAny`, // write
+        `${collection}.retrieve`, // read
+        `${collection}.update`, // write
     );
 
     if (op.get(params, 'user')) {
@@ -1659,8 +1659,8 @@ Content.setPermissions = async (params, options) => {
         let userId = op.get(contentObj, 'user.objectId');
         const groupACL = await Actinium.Utils.CloudACL(
             permissions,
-            `${typeObj.collection}.retrieveAny`,
-            `${typeObj.collection}.updateAny`,
+            `${typeObj.collection}.retrieve`,
+            `${typeObj.collection}.update`,
         );
 
         if (userId) {
