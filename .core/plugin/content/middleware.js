c2b760f897e96050b7dde232544e02d6bac1ce47
John Dillick <john@dillick.us>
2020-10-24 23:17:22 -0400

    Add content middleware to help with caching.

---
+++ .core/plugin/content/middleware.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const request = require('request');
+const op = require('object-path');
+const _ = require('underscore');
+
+console.log('registering content routes');
+
+/**
+ * Simple no-privilege express routes for content
+ */
+Actinium.Middleware.register(
+    'content-helper',
+    app => {
+        const router = express.Router();
+        router.use('/content/:type/:id', async (req, res) => {
+            const { type: machineName, id: objectId } = req.params;
+            try {
+                const content = await Actinium.Content.retrieve({
+                    type: {
+                        machineName,
+                    },
+                    objectId,
+                    current: true,
+                    resolveRelations: true,
+                });
+
+                res.json(content);
+            } catch (error) {
+                res.status(500).json(error);
+            }
+        });
+
+        app.use(router);
+
+        return Promise.resolve();
+    },
+    -10000000,
+);
