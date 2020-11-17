16ca40c92d1f21800d3f47d8cdf23c373b037f60
John Dillick <john@dillick.us>
2020-11-16 19:23:33 -0500

    Remove MenuBuilder plugin now that they have fly-weight content.

--- .core/plugin/content/MenuBuilder/info.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    ID: 'MenuBuilder',
-    description: 'Maintain MenuBuilder content FieldType',
-    name: 'Menu Builder field maintenance',
-    order: 100,
-    meta: {
-        builtIn: true,
-    },
-};
