eef2bf9573df3cc553aab8b8a3dd4fd08376e7fb
John Dillick <john@dillick.us>
2023-05-04 18:08:15 -1000

    .core is no longer needed

--- .core/lib/StdOutLoggerAdapter.js
+++
@@ -1,8 +0,0 @@
-class StdOutLoggerAdapter {
-    constructor(options) {}
-    log(level, message) {
-        BOOT(message);
-    }
-}
-
-export default StdOutLoggerAdapter;
--- .core/middleware/morgan/middleware.js
+++
@@ -1,9 +0,0 @@
-import morgan from 'morgan';
-
-Actinium.Middleware.register('morgan', app => {
-    if (process.env.NODE_ENV !== 'development') {
-        app.use(morgan('combined'));
-    }
-
-    return Promise.resolve();
-});
